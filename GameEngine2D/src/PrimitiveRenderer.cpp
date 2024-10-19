3607e5e86980f1b456a48c4e5222184e94bae48f
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-10-19 14:42:47 +0200

    Change drawLine implementation

--- GameEngine2D/src/PrimitiveRenderer.cpp
+++ GameEngine2D/src/PrimitiveRenderer.cpp
@@ -9,22 +9,36 @@ sf::Image PrimitiveRenderer::drawPoint(Point2D point, ColorRGB color, sf::Image 
     return image;
 }
 
-// Implementation of drawLine using incremental algorithm
 sf::Image PrimitiveRenderer::drawLine(Point2D p1, Point2D p2, ColorRGB color, sf::Image& image) {
     double dx = p2.x - p1.x;
     double dy = p2.y - p1.y;
-    double m = dy / dx;
 
-    if (m < 1 && m > -1) {
+    if (getAbsolute(dx) > getAbsolute(dy)) {
+        if (p1.x > p2.x) {
+            std::swap(p1, p2);
+            dx = p2.x - p1.x;
+            dy = p2.y - p1.y;
+        }
+
+        double m = dy / dx;
+        double y = p1.y;
+
         for (int x = p1.x; x <= p2.x; x++) {
-            int y = p1.y + m * (x - p1.x);
             image.setPixel(x, y, sf::Color(color.r, color.g, color.b));
+            y += m;
         }
     } else {
-        m = dx / dy;
+        if (p1.y > p2.y) {
+            std::swap(p1, p2);
+            dx = p2.x - p1.x;
+            dy = p2.y - p1.y;
+        }
+
+        double m = dx / dy;
+        double x = p1.x;
         for (int y = p1.y; y <= p2.y; y++) {
-            int x = p1.x + m * (y - p1.y);
             image.setPixel(x, y, sf::Color(color.r, color.g, color.b));
+            x += m;
         }
     }
 

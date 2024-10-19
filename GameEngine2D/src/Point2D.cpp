b336f0fc355cf2972beacb83d4e61b85c5982054
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-10-19 16:49:00 +0200

    Add new constructor to Point2D

--- GameEngine2D/src/Point2D.cpp
+++ GameEngine2D/src/Point2D.cpp
@@ -2,6 +2,10 @@
 
 #include <cmath>
 
+Point2D::Point2D() {
+    x = 0;
+    y = 0;
+}
 
 Point2D::Point2D(float xInput, float yInput){
     x = round(xInput);
--- GameEngine2D/src/Point2D.h
+++ GameEngine2D/src/Point2D.h
@@ -1,11 +1,9 @@
-
-
 class Point2D{
     public:
         int x;
         int y;
     
     public:
+        Point2D();
         Point2D(float xInput, float yInput);
-
 };

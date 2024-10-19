8379e5cb8f3629e88172a86f208bf6ff5d97e62b
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-10-19 16:53:36 +0200

    Created LineSegment class

---
+++ GameEngine2D/src/LineSegment.cpp
@@ -0,0 +1,8 @@
+#include "./LineSegment.h"
+#include "./Point2D.h"
+
+
+LineSegment::LineSegment(Point2D p1, Point2D p2) {
+	this->p1 = p1;
+	this->p2 = p2;
+}
---
+++ GameEngine2D/src/LineSegment.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "./Point2D.h"
+
+
+class LineSegment {
+	public:
+		Point2D p1;
+		Point2D p2;
+
+	public:
+		LineSegment(Point2D p1, Point2D p2);
+};

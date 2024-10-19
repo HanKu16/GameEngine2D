5a2ecb86132f2043721de8f28842278ad0c71a98
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-10-19 18:50:36 +0200

    Modified drawOpenLine and drawClosedLine declarations

--- GameEngine2D/src/PrimitiveRenderer.h
+++ GameEngine2D/src/PrimitiveRenderer.h
@@ -18,8 +18,8 @@ class PrimitiveRenderer{
         sf::Image drawLine(Point2D p1, Point2D p2, ColorRGB color, sf::Image& image);
         sf::Image drawLineUsingDDA(LineSegment line, ColorRGB color, sf::Image& image);
         sf::Image drawLineUsingDefault(const LineSegment& line, ColorRGB color, sf::Image& image);
-        sf::Image drawOpenLine(std::vector<Point2D> points, ColorRGB color);
-        sf::Image drawClosedLine(std::vector<Point2D> points, ColorRGB color);
+        void drawOpenLine(std::vector<Point2D> points, ColorRGB color, sf::Image& image);
+        sf::Image drawClosedLine(std::vector<Point2D> points, ColorRGB color, sf::Image& image);
         sf::Image drawDashedLine(std::vector<LineSegment> lines, ColorRGB color, sf::Image& image);
         sf::Image drawRectangle(Point2D p1, Point2D p2, Point2D p3, Point2D p4,
             ColorRGB color, sf::Image& image);

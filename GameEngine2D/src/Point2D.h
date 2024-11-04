5ca44ce5366780a638c1f177dce7cbfd986e9448
MexerGG <102914216+MexerGG@users.noreply.github.com>
2024-11-04 20:50:55 +0100

    Update Point2D.h

--- GameEngine2D/src/Point2D.h
+++ GameEngine2D/src/Point2D.h
@@ -8,4 +8,9 @@ class Point2D{
     public:
         Point2D();
         Point2D(float xInput, float yInput);
+
+        void draw(sf::RenderWindow& window) override;
+        void translate(float dx, float dy) override;
+        void rotate(float angle) override;
+        void scale(float factor) override;
 };
